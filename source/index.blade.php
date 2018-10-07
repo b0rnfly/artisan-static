8375a9658d53f210a65861ebf9f1fa6e2789917e
gregory <34366772+b0rnfly@users.noreply.github.com>
2018-10-07 15:56:56 -0500

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,11 +1,9 @@
 @extends('_layouts.master')
 
 @section('content')
-    <h1>Welcome to Artisan Static for Jigsaw</h1>
+    <h1>fagslog:</h1>
 
-    <h2>Asset Compilation Test</h2>
+    <span class="test-css">rude_gay</span></p>
 
-    <p>CSS test: <span class="test-css">text with border and padding</span></p>
-
-    <p>JavaScript test: <a href="#" class="test-js">click me</a></p>
+ 
 @endsection
