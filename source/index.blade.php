153d91fc49a72b2cb0f9775027db722091ce8442
gregory <34366772+b0rnfly@users.noreply.github.com>
2018-10-10 11:24:59 -0500

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,7 +1,7 @@
 @extends('_layouts.master')
 
 @section('content')
- <ul>  <h1> <center> <span class="test-css">rude_gay</span></p></h1>
+ <topnav> <ul> </li><h1> <center> <span class="test-css">rude_gay</span></p></h1></li>
 
 </ul>
  
